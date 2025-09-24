6962a0ebfa371a0336df326c310a3e49ec828ce7
IsraelDev-var <israel230804vc@gmail.com>
2025-09-23 20:25:17 -0400

    Configuracion de la conection de la basa de dato y migracion inicial

---
+++ Persistence/Contexts/AppContextDB.cs
@@ -0,0 +1,43 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using Persistence.Entities;
+
+using System.Reflection;
+
+namespace Persistence.Contexts
+{
+    public class AppContextDB : DbContext
+    {
+        public AppContextDB(DbContextOptions<AppContextDB> options): base (options) { }
+
+
+        // Configuracion de las entidades
+        public DbSet<Country> Counties { get; set; }
+
+        public DbSet<Macroindicator> Macroindicators { get; set; }
+        public DbSet<IndicatorsByCountry> IndicatorsByCountries { get; set; }
+        public DbSet<ReteOfReturn> RateOfReturns { get; set; }
+        //public DbSet<RankingResult> RankingResults { get; set; } Esta tabla no se almacenara
+        public DbSet<MacroindicatorSimulation> MacroindicatorSimulations { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+
+            //modelBuilder.ApplyConfiguration(new CountryEntityConfig());
+            //modelBuilder.ApplyConfiguration(new IndicatorsByCountryEntityConfig());
+            //modelBuilder.ApplyConfiguration(new MacroindicatorEntityConfig());
+            //modelBuilder.ApplyConfiguration(new MacroindicatorSimulationEntityConfig());
+            //modelBuilder.ApplyConfiguration(new ReteOfReturnEntityConfig());
+
+        } 
+
+        
+        
+
+        
+    }
+}
